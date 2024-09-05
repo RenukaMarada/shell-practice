436b2e9d147fd9e48b38eebc0eabd9f57d1a0594
RenukaMarada <renuka.marada09@gmail.com>
2024-09-05 12:53:05 +0530

    Script using variables

--- 01-basic-scripts.sh
+++ 01-basic-scripts.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
+# Basic Hello Worl script
+
 # echo "Hello World"
 
-echo "Ramesh:: Hi Suresh, How are you?"
-echo "Suresh:: Hello Ramesh. I am fine. How are you doing?"
-echo "Ramesh:: I am doing good Suresh. What's going on?"
-echo "Suresh:: I started learning Shell Script Ramesh"
+# Script without variables
+
+# echo "Ramesh:: Hi Suresh, How are you?"
+# echo "Suresh:: Hello Ramesh. I am fine. How are you doing?"
+# echo "Ramesh:: I am doing good Suresh. What's going on?"
+# echo "Suresh:: I started learning Shell Script Ramesh"
+
+# script using variables
+
+Person1=Ramesh
+Person2=Suresh
+
+echo "$Person1:: Hi $Person2, How are you?"
+echo "$Person2:: Hello $Person1. I am fine. How are you doing?"
+echo "$Person1:: I am doing good $Person2. What's going on?"
+echo "$Person2:: I started learning Shell Script $Person1"
