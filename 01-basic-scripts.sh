bf2d0c37d27442925add2dfc0fd5b4b7ebae0403
RenukaMarada <renuka.marada09@gmail.com>
2024-09-05 13:01:39 +0530

    Script for accepting input from user

--- 01-basic-scripts.sh
+++ 01-basic-scripts.sh
@@ -23,10 +23,22 @@
 
 # Script for using command line arguments
 
-Person1=$1
-Person2=$2
+# Person1=$1
+# Person2=$2
 
-echo "$Person1:: Hi $Person2, How are you?"
-echo "$Person2:: Hello $Person1. I am fine. How are you doing?"
-echo "$Person1:: I am doing good $Person2. What's going on?"
-echo "$Person2:: I started learning Shell Script $Person1"
+# echo "$Person1:: Hi $Person2, How are you?"
+# echo "$Person2:: Hello $Person1. I am fine. How are you doing?"
+# echo "$Person1:: I am doing good $Person2. What's going on?"
+# echo "$Person2:: I started learning Shell Script $Person1"
+
+# Accepting input from User
+
+echo "Please enter your username::"
+read USERNAME
+
+echo "Username entered is: $USERNAME"
+
+echo "Please enter your password::" # Hides password
+read -s PASSWORD
+
+echo "Password entered is: $PASSWORD"
