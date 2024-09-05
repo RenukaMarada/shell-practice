26cbc228eed22ab9cfdba421d3786f8b8c0b9a7c
RenukaMarada <renuka.marada09@gmail.com>
2024-09-05 13:46:13 +0530

    scripts for variables, datatypes and calculator

--- 01-variables.sh
+++ 01-variables.sh
@@ -2,34 +2,34 @@
 
 # Basic Hello Worl script
 
-# echo "Hello World"
+echo "Hello World"
 
-# Script without variables
+Script without variables
 
-# echo "Ramesh:: Hi Suresh, How are you?"
-# echo "Suresh:: Hello Ramesh. I am fine. How are you doing?"
-# echo "Ramesh:: I am doing good Suresh. What's going on?"
-# echo "Suresh:: I started learning Shell Script Ramesh"
+echo "Ramesh:: Hi Suresh, How are you?"
+echo "Suresh:: Hello Ramesh. I am fine. How are you doing?"
+echo "Ramesh:: I am doing good Suresh. What's going on?"
+echo "Suresh:: I started learning Shell Script Ramesh"
 
 # script using variables
 
-# Person1=Ramesh
-# Person2=Suresh
+Person1=Ramesh
+Person2=Suresh
 
-# echo "$Person1:: Hi $Person2, How are you?"
-# echo "$Person2:: Hello $Person1. I am fine. How are you doing?"
-# echo "$Person1:: I am doing good $Person2. What's going on?"
-# echo "$Person2:: I started learning Shell Script $Person1"
+echo "$Person1:: Hi $Person2, How are you?"
+echo "$Person2:: Hello $Person1. I am fine. How are you doing?"
+echo "$Person1:: I am doing good $Person2. What's going on?"
+echo "$Person2:: I started learning Shell Script $Person1"
 
 # Script for using command line arguments
 
-# Person1=$1
-# Person2=$2
+Person1=$1
+Person2=$2
 
-# echo "$Person1:: Hi $Person2, How are you?"
-# echo "$Person2:: Hello $Person1. I am fine. How are you doing?"
-# echo "$Person1:: I am doing good $Person2. What's going on?"
-# echo "$Person2:: I started learning Shell Script $Person1"
+echo "$Person1:: Hi $Person2, How are you?"
+echo "$Person2:: Hello $Person1. I am fine. How are you doing?"
+echo "$Person1:: I am doing good $Person2. What's going on?"
+echo "$Person2:: I started learning Shell Script $Person1"
 
 # Accepting input from User
 
--- 02-datatypes.sh
+++ 02-datatypes.sh
@@ -10,3 +10,23 @@ echo "Capital of TS is: ${Capitals[1]}"
 echo "Capital of Chennai is: ${Capitals[2]}"
 
 echo "all capitals: ${Capitals[@]}"
+
+# To print date with using date command
+
+DATE=$(date +%F)
+echo "Today's date: $DATE"
+
+# Calculator script
+
+num1=$1
+num2=$2
+
+sum=$(($1 + $2))
+sub=$(($1 - $2))
+mul=$(($1 * $2))
+div=$(($1 / $2))
+
+echo "sum of $num1 and $num2 is:$sum"
+echo "substraction of $num1 and $num2 is:$sub"
+echo "multiplication of $num1 and $num2 is:$mul"
+echo "division of $num1 and $num2 is:$div"
